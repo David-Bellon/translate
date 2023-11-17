57c1319072583b32306bf82032ed151012acc56e
David-Bellon <91338053+David-Bellon@users.noreply.github.com>
2023-11-18 00:41:03 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ with open("spa-eng/spa.txt") as f:
     en = []
     es = []
     for line in f:
-        x = line.translate(str.maketrans('', '', string.punctuation)).replace("¿", "").replace("¡", "")
+        x = line.translate(str.maketrans('', '', string.punctuation)).replace("¿", "").replace("¡", "").lower()
         _en, _es = x.split("\t")[:2]
         en.append(_en)
         es.append(_es)
--- tokenizer.py
+++ tokenizer.py
@@ -1,5 +1,20 @@
-from transformers import DistilBertTokenizer, AutoTokenizer
 import pandas as pd
 
 df = pd.read_csv("translate.csv")
-tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
+words_en = set()
+words_es = set()
+for line in df["En"]:
+    for word in line.split(" "):
+        words_en.add(word)
+for line in df["Es"]:
+    for word in line.split(" "):
+        words_es.add(word)
+
+print(words_en)
+with open("en_vocab.txt", "a") as f:
+    for i, w in enumerate(list(words_en)[1:]):
+        f.write(w + " " + str(i) + "\n")
+
+with open("es_vocab.txt", "a") as f:
+    for i, w in enumerate(list(words_es)[1:]):
+        f.write(w + " " + str(i) + "\n")
